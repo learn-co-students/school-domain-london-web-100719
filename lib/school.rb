c4432eac12d37cbfeadd03a5c33afa355a8348e9
gortron <gordon.lanza@gmail.com>
2019-10-08 14:29:29 +0100

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,29 @@
-# code here!
+class School
+
+  def initialize(school_name)
+    @school_name = school_name
+    @roster = {}
+  end
+
+  def roster
+    @roster
+  end
+
+  def add_student(name, age)
+    if @roster.keys.include?(age)
+      @roster[age] << name
+    else
+      @roster[age] = []
+      @roster[age] << name
+    end
+  end
+
+  def grade(age)
+    @roster[age]
+  end
+
+  def sort
+    @roster.map{|grades, students| @roster[grades] = students.sort}
+    @roster
+  end
+end
