1491f82420329292b8e19ef4c0c8273a8effb75f
geewey <geewey@gmail.com>
2019-10-09 16:57:30 +0100

    Update school.rb

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,31 @@
-# code here!
+require "pry"
+
+class School
+
+    def initialize(name)
+        @name = name
+    end
+   
+    def roster
+        @roster = {}
+    end
+
+    def add_student(student_name, grade)
+        @student_name = student_name
+        @grade = grade
+
+        if @roster[grade]
+            @roster[grade] ||= []
+        
+        @roster[grade].push(student_name)
+    end
+
+    def grade(grade)
+        @roster[grade]
+    end
+
+    def sort
+        @roster.sort_by_value
+    end
+    
+end
