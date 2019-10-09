d09752deb51a6fda1a7d7220fb81381293e03f5f
Tish Chungoora <tishal82@gmail.com>
2019-10-09 11:47:01 +0100

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,26 @@
-# code here!
+class School
+    attr_reader :roster
+
+    def initialize(name)
+        @name = name
+        @roster = {}
+    end
+
+    def add_student(name, grade)
+        @roster[grade] ||= []
+        @roster[grade] << name
+    end
+
+    def grade(num)
+        @roster[num]
+    end
+
+    def sort
+        sorted = {}
+        sorted =  sorted.merge(@roster)
+        sorted.each do |grade, names|
+            sorted[grade] = names.sort
+            sorted = {grade => sorted[grade]}
+        end
+    end
+end
