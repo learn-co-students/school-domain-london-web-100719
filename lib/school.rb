3b2dd77aa05b8665a2830768a50ba5bd3352d2f6
geewey <geewey@gmail.com>
2019-10-09 17:11:31 +0100

    Update school.rb

--- lib/school.rb
+++ lib/school.rb
@@ -13,17 +13,17 @@ class School
     def add_student(student_name, grade)
         @student_name = student_name
         @grade = grade
-        
-        @roster[grade] ||= []
+
+        @roster[grade] ||= [student_name]
         @roster[grade] << student_name
     end
 
-    def grade(grade)
-        @roster[grade]
-    end
+    # def grade(grade)
+    #     @roster[grade]
+    # end
 
-    def sort
-        @roster.sort_by_value
-    end
+    # def sort
+    #     @roster.sort_by_value
+    # end
     
 end
